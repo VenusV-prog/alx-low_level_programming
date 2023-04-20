2c253f3ea0e7dee8ceb2156a4e063a7fb96cb679
Venus <tadonkevenus@gmail.com>
2023-04-20 15:35:03 -0700

    print with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,15 +3,14 @@
 /**
  * main - prints "with proper grammar,
  * but the outcome
- * is a piece of art, followed by a new line
- * Return: program should return 0
+ * is a piece of art," followed newline
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
-
 	return (0);
 
 }
