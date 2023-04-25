b386eb4cd564c95912f264102741a82b20146880
Venus <tadonkevenus@gmail.com>
2023-04-24 19:41:25 -0700

    print base 16 numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all numbers of base 16 in lowercase 
+ * main - Prints all numbers of base 16 in lowercase
  * that is numbers between 0 to 9 and letters between a to f.
  * Return: Always 0 (Success)
  */
