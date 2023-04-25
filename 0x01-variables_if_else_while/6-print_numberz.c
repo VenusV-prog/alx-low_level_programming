a6b325f8b725d18b9c03e95d1473e5e6677b84bd
Venus <tadonkevenus@gmail.com>
2023-04-24 19:29:47 -0700

    prints single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of base 10 from 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 58; i < 68; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
